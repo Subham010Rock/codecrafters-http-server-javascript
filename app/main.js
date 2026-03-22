901c776a0a614fdd3a9b09078197f7b08a2471a3
Subham Kumar <subhamk@argusoft.com>
2026-03-23 02:22:21 +0530

    send message to client

--- app/main.js
+++ app/main.js
@@ -8,6 +8,7 @@ const server = net.createServer((socket) => {
   socket.on("close", () => {
     socket.end();
   });
+  socket.write("Http/1.1 200 OK\r\n\r\n");
 });
 //
 server.listen(4221, "localhost");
